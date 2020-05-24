4165e4793ea1935fc99932519494afacd5e88ff8
sidmonta <sidmonta.15@gmail.com>
2020-05-24 12:16:45 +0200

    Comment of Crawler methods

--- src/Crawler.ts
+++ src/Crawler.ts
@@ -1,17 +1,32 @@
-import { identity, pipe, includes, isNil, curry } from 'ramda'
+import { identity, pipe, includes, isNil, equals } from 'ramda'
 import { Quad, Store, N3Store } from 'n3'
 import { Observable, Subject, Subscription } from 'rxjs'
 import { filter, mergeMap } from 'rxjs/operators'
-import { Lod, Rx } from '@sidmonta/babelelibrary'
+import { Lod, Rx, Services } from '@sidmonta/babelelibrary'
 import extractDomain from 'extract-domain'
 import { allCheck } from './changeUri'
 
+/**
+ * Semplice alias per definire il tipo di una URI
+ */
 type URI = string
 
+/**
+ * Definisce la tipologia della funzione che controlla le uri
+ */
 export type CheckURI = (uri: URI) => URI
 
+/**
+ * Alias della funzione getID della libreria per estrarre l'id da una URI
+ * @param uri uri da cui estrarre l'identificativo
+ */
 const getID = (uri: URI) => Lod.getID(uri) || ''
 
+/**
+ * Genera un filtro per uno stream RxJs che dato uno id di un'URI filtra tutte le triple che contengono
+ * quell'identificativo
+ * @param id identificativo di una URI
+ */
 const filterQuadByIncludedService = (id: URI) => filter((quad: Quad) => quad.subject.value.includes(id))
 
 /**
@@ -27,7 +42,10 @@ const checkSameAs: (node: Quad) => URI = (node: Quad): URI =>
     ? node.object.value
     : ''
 
-
+/**
+ * Funzione che determina se una tripla è una tripla vuota
+ * @param quad tripla da verificare
+ */
 export const filtQuad = (quad: Quad): boolean =>
   !equals('BlankNode', quad.subject.termType) &&
   !equals(
@@ -57,42 +75,91 @@ export const filtQuad = (quad: Quad): boolean =>
  * Il Crawler registra uno store di triple per poter lavora più agilmente su tutte le triple trovate
  */
 export default class Crawler {
-  private history: Set<URI> = new Set<URI>()
+  /**
+   * Salva la lista di identificativi di un soggetto così da non caricare più volte le stesse informazioni.
+   * In pratica risolve il problema dei cappi nel grafo delle risorse dei RDF
+   */
   private historyID: Set<string> = new Set<string>()
 
+  /**
+   * Repository che contiene tutte le triple analizzate per un soggetto.
+   * Questo registro è utile perché l'oggetto N3Store mette a disposizione dei metodi per navigare agilmente il registro
+   */
   public quadStore: N3Store = new Store()
 
+  /**
+   * Soggetto, cioè l'emettitore degli eventi, per le triple che contengono <sameAs>
+   */
   private sameAs: Subject<URI> = new Subject<URI>()
+  /**
+   * Soggetto, cioè l'emettitore degli eventi, per tutte le triple ricavate dalla scansione
+   */
   private quadUpcoming: Subject<Quad> = new Subject<Quad>()
 
+  /**
+   * Stream delle triple che contengono il predicato <sameAs>
+   */
   public sameAs$: Observable<URI> = this.sameAs.asObservable()
+  /**
+   * Stream delle triple che vengono ricavate durante la scansione
+   */
   public quadUpcoming$: Observable<Quad> = this.quadUpcoming.asObservable()
 
+  /**
+   * Stream di tutte le chiamate fetch ai servizi.
+   */
   private download$: Observable<Quad>
+  /**
+   * Oggetto che contiene la sottoscrizione allo stream delle chiamate fetch ai servizi
+   */
   private subscribeDownload: Subscription
 
-  constructor(checkUri: CheckURI = identity) {
-    const checkURIForDownload = pipe(allCheck, checkUri)
-    const fetchURI = pipe(checkURIForDownload, Rx.fatchSPARQL)
+  /**
+   * Plugin da utilizzare per cachere le risorse che vengono salvate.
+   */
+  private cachePlugin: Services.CachePlugin<URI, void>
 
+  /**
+   * Costruttore del Crawler
+   * @param [checkUri] funzione opzionale che trasforma le URI nella versione per ritornare il file RDF
+   */
+  constructor(checkUri: CheckURI = identity) {
+    // Unisco alla funzione personalizzata dell'utente, le modifiche alle URL per i servizi Wikidata e VIAF
+    const checkURIForDownload = pipe(allCheck, checkUri)
+    // Funzione che si fa la fetch al servizio LOD per una determinata URI
+    // Prima applica le modifiche all'URI per il servizio richiesto
+    const fetchURI = pipe(checkURIForDownload, Rx.fatchSPARQL)
+    // Istanzia una cache di default. Il plugin può essere sostituita in seguito
+    this.cachePlugin = new Services.InMemoryPlugin<string, void>()
+
+    /**
+     * Funzione che si occupa di fare la chiamata all'URI, filtrando le triple ritornate solo per quelle che
+     * corrispondono all'identificativo del soggetto e che non sono vuote.
+     * @param uri
+     */
     const donwloadRDF = (uri: URI) => {
       const id = getID(uri)
-      this.historyID.add(id)
+      this.historyID.add(id) // Mi salvo l'identificativo cercato per non cercarlo di nuovo successivamente.
       return fetchURI(uri).pipe(
-        filterQuadByIncludedService(id),
-        filter(filtQuad)
+        filterQuadByIncludedService(id), // Filtro le triple solo che appartengono all'identificativo del soggetto
+        filter(filtQuad) // Filtro le triple che non sono vuote.
       )
     }
 
+    /**
+     * Genero lo stream dei download a partire dallo stream di triple <sameAs> che non ho già analizzato.
+     */
     this.download$ = this.sameAs$.pipe(
-      filter((uri: URI) => !this.historyID.has(getID(uri))),
-      mergeMap(donwloadRDF)
+      filter((uri: URI) => !this.historyID.has(getID(uri))), // Filtro solo i sameAs già analizzati
+      mergeMap(donwloadRDF) // Passo lo stream dei <sameAs> con lo stream delle chiamate fetch
     )
 
+    // Sottoscrivo allo streaming delle chiamate
     this.subscribeDownload = this.download$.subscribe((quad: Quad) => {
-      const sameAs = checkSameAs(quad)
+      const sameAs = checkSameAs(quad) // Recupero, se presente il valore del predicato <sameAs>
+      // Controllo se è presente il tag <sameAs> e se non ho già analizzato quel servizio
       if (sameAs && !this.sameDomain(sameAs)) {
-        this.sameAs.next(sameAs)
+        this.sameAs.next(sameAs) // Genero un nuovo evento nello stream dei sameAs
       }
 
       // Aggiungo le informazioni al quadUpcoming e allo store di triple
@@ -118,8 +185,8 @@ export default class Crawler {
    * @param {string} [fil] regex per filtrare le query che arrivano e ottenere solo quelle desiderate.
    * Parametro opzionale
    */
-  onNewNode(callback: (v: Quad) => void, fil?: string): void {
-    let filterQuad = fil ? Lod.checkQuad(fil) : curry(_ => true)
+  public onNewNode(callback: (v: Quad) => void, fil?: string): void {
+    let filterQuad = fil ? Lod.checkQuad(fil) : _ => true
     this.quadUpcoming$
       .pipe(filter((q: Quad) => q && filterQuad(q)))
       .subscribe(callback)
@@ -129,32 +196,50 @@ export default class Crawler {
    * Metodo/evento per essere notificati ogni qual volta si è trovata una nuova base dati LOD
    * @param {(url: string) => void)} callback funzione da eseguire ogni qualvolta si è trovata una nuova banca dati da scansionare
    */
-  onNewSource(callback: (url: string) => void): void {
+  public onNewSource(callback: (url: string) => void): void {
     this.sameAs$.subscribe(callback)
   }
 
-  getNewNodeStream(fil?: string) {
-    let filterQuad = fil ? Lod.checkQuad(fil) : curry(_ => true)
+  /**
+   * Ritorna lo stream in cui ogni evento è una tripla trovata durante la ricerca.
+   * @param fil Filtra le triple solo se contengono il valore di fil
+   */
+  public getNewNodeStream(fil?: string) {
+    let filterQuad = fil ? Lod.checkQuad(fil) : _ => true
     return this.quadUpcoming$.pipe(filter((q: Quad) => q && filterQuad(q)))
   }
 
-  getNewSourceStream() {
+  /**
+   * Ritorna lo stream per le triple <sameAs> che corrispondono ai servizi analizzati durante la navigazione
+   */
+  public getNewSourceStream() {
     return this.sameAs$
   }
 
   /**
    * Interrompe il crawler
    */
-  end(): void {
+  public end(): void {
     this.subscribeDownload.unsubscribe()
     this.quadUpcoming.unsubscribe()
     this.sameAs.unsubscribe()
   }
 
-  clear(): void {
+  /**
+   * Re-inizializza il crawler resettando i vari registri di informazioni
+   */
+  public clear(): void {
     this.quadStore = new Store()
-    this.history = new Set<URI>()
     this.historyID = new Set<string>()
+    this.cachePlugin.flush()
+  }
+
+  /**
+   * Imposta il plugin di cache da utilizzare durante la navigazione
+   * @param plugin plugin per la cache
+   */
+  public setCache(plugin: Services.CachePlugin<string, never>): void {
+    this.cachePlugin = plugin
   }
 
   /**
@@ -163,12 +248,12 @@ export default class Crawler {
    * @returns {boolean} se ho già analizzato qulla URI
    */
   private sameDomain(domain: URI): boolean {
-    let dom = extractDomain(domain)
+    let dom = extractDomain(domain) // estrae il dominio di dall'URI
 
-    if (isNil(dom) || this.history.has(dom)) {
+    if (isNil(dom) || this.cachePlugin.has(dom)) {
       return true
     } else {
-      this.history.add(dom)
+      this.cachePlugin.add(dom)
       return false
     }
   }
